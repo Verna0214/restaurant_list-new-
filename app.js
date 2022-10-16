91cddde1ff9227f09c6cc058a012b7bb6906519c
Verna0214 <yuhsichen0214@gmail.com>
2022-10-16 16:04:22 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const routes = require('./routes')
 
-// 載入 Restaurant model
-const Restaurant = require('./models/restaurant')
+const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
-// 設定連線mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得mongoDB連線狀態
-const db = mongoose.connection
-// 連線異常警示
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
