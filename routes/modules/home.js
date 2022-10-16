92b15e410861bf6ef5f461125f9d9204149594cf
Verna0214 <yuhsichen0214@gmail.com>
2022-10-16 22:28:33 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,23 +1,36 @@
 const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
+const sorting = require('../../utilities/sort')
+
 
 router.get('/', (req, res) => {
   Restaurant.find() // 取出Restaurant model裡的資料
     .lean()
-    .sort({ _id: 'asc' })
+    .sort({ _id: 'desc' })
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
 
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  Restaurant.find()
+  const sortingType = req.query.sort
+  const typeObject = {
+    isOne: sortingType === '1',
+    isTwo: sortingType === '2',
+    isThree: sortingType === '3',
+    isFour: sortingType === '4'
+  }
+  Restaurant.find({})
     .lean()
-    .then(restaurants => {
-      const restaurantsData = restaurants.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()) || item.category.includes(keyword))
-      res.render('index', { restaurants: restaurantsData, keyword: keyword })
+    .sort(sorting(sortingType))
+    .then(restaurantsData => {
+      const filterRestaurants = restaurantsData.filter(restaurant => {
+        return restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name.includes(keyword) || restaurant.category.includes(keyword)
+      })
+      res.render('index', { restaurants: filterRestaurants, keyword, typeObject })
     })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
